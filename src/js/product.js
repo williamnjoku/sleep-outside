2b8ffa311d6c797c190b22df1c04f8834a99b15d
williamnjoku <njokuwilliam19@gmail.com>
2025-11-14 15:23:17 +0100

    New commit

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
 
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import ProductDetails from "./productDetails.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
